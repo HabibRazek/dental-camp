b442bd4ad2dee2b73e193f3b93840acbefdba9a2
HabibRazek <habibrazeg32@gmail.com>
2025-04-13 21:56:17 +0100

    v-1

--- prisma.ts
+++ prisma.ts
@@ -1,7 +1,15 @@
-import { PrismaClient } from "@prisma/client"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { PrismaClient } from '@prisma/client'
+
+type GlobalThisWithPrisma = typeof globalThis & {
+    __prisma?: PrismaClient
+}
+
+const globalWithPrisma = globalThis as GlobalThisWithPrisma
+
+const prisma = globalWithPrisma.__prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+    globalWithPrisma.__prisma = prisma
+}
+
+export default prisma
