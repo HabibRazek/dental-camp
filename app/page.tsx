b0ebcf7258a7788b83fcf67ba29008909a10c142
HabibRazek <habibrazeg32@gmail.com>
2025-04-15 20:30:35 +0100

    setup prisma

--- app/page.tsx
+++ app/page.tsx
@@ -8,3 +8,26 @@ export default function Home() {
     </div>
   );
 }
+// import Landing from "./components/landing/landing";
+
+// import { PrismaClient } from "@/src/app/generated/prisma";
+
+
+
+
+
+// export default async function Home() {
+//   const prisma = new PrismaClient()
+//   const users = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//     },
+//   });
+//   console.log(users);
+//   return (
+    
+//     <div>
+//         {/* <Landing /> */}
+//     </div>
+//   );
+
